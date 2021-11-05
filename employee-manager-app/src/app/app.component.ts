84d57fb6ce0faafbf58a752b5463b6674d203912
Marten Hernebring <marten.hernebring@gmail.com>
2021-11-05 10:05:37 +0100

    Created add employee form when clicking on add employee nav bar button

--- employee-manager-app/src/app/app.component.ts
+++ employee-manager-app/src/app/app.component.ts
@@ -27,4 +27,19 @@ export class AppComponent implements OnInit {
           }
       )
   }
+
+  public onOpenModal(mode: string):void {
+    const container = document.getElementById('main-container')
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    //TODO add parameter employee: Employee before mode above
+    //TODO if mode for edit and delete
+    container?.appendChild(button);
+    button.click();
+  }
 }
