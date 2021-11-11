349a8a9fdff79d2c0390948f0ac96193044c5d07
Marten Hernebring <marten.hernebring@gmail.com>
2021-11-11 14:57:55 +0100

    Get all was working fine

--- employee-manager-app/src/app/employee.service.ts
+++ employee-manager-app/src/app/employee.service.ts
@@ -9,16 +9,16 @@ export class EmployeeService {
     private apiServerUrl = environment.apiBaseUrl;
     constructor(private http: HttpClient) {}
 
-    public getEmployees(): Employee[] {
-        //return this.http.get<Employee[]>(`${this.apiServerUrl}/v1/employees/`);
-        const arr: Employee[] = [
+    public getEmployees(): Observable<Employee[]> {
+        const arr = this.http.get<Employee[]>(`${this.apiServerUrl}/v1/employees`);
+        /*const arr: Employee[] = [
             {
-            employeeId: 'anaand1234',
+            userId: 'anaand1234',
             firstName: 'Ana',
             lastName: 'Andersson',
             personalNumber: '900719-XXXX',
             email: 'ana@gmail.com',
-            phone: '012345678',
+            phoneNumber: '012345678',
             street: 'Anagatan 3',
             zip:'123-45',
             city: 'Göteborg',
@@ -26,8 +26,8 @@ export class EmployeeService {
             parentCompany: undefined,
             startDate: new Date(),
             endDate: undefined,
-            imageUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-        }]
+            //imageUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+        }]*/
         return arr;
     }
 
@@ -35,12 +35,12 @@ export class EmployeeService {
        //return this.http.post<Employee>(`${this.apiServerUrl}/v1/employees/`, employee);
        public addEmployee(employee: Employee): Employee {
         const emp: Employee = {
-            employeeId: 'anaand1234',
+            userId: 'anaand1234',
             firstName: 'Ana',
             lastName: 'Andersson',
             personalNumber: '900719-XXXX',
             email: 'ana@gmail.com',
-            phone: '012345678',
+            phoneNumber: '012345678',
             street: 'Anagatan 3',
             zip:'123-45',
             city: 'Göteborg',
@@ -48,17 +48,17 @@ export class EmployeeService {
             parentCompany: undefined,
             startDate: new Date(),
             endDate: undefined,
-            imageUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+            //imageUrl: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
         }
 
         return emp
     }
 
     /*public updateEmployee(employee: Employee): Observable<Employee> {
-        return this.http.put<Employee>(`${this.apiServerUrl}/employees/${employee.employeeId}`, employee);
+        return this.http.put<Employee>(`${this.apiServerUrl}/employees/${employee.userId}`, employee);
     }*/
 
-    /*public deleteEmployee(employeeId: number): Observable<void> {
-        return this.http.delete<void>(`${this.apiServerUrl}/employees/${employeeId}`);
+    /*public deleteEmployee(userId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/employees/${userId}`);
     }*/
 }
