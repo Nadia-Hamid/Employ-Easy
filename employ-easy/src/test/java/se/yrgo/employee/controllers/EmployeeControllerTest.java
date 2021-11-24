dae78e8ed1b4858e102500339a25c5bc7dd0a079
Marten Hernebring <marten.hernebring@gmail.com>
2021-11-24 15:03:34 +0100

    Added test for greeting endpoint

--- employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
+++ employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
@@ -1,36 +1,50 @@
 package se.yrgo.employee.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class EmployeeControllerTest {
-//
-//    @Autowired
-//    private WebApplicationContext context;
-//
-//    private MockMvc mockMvc;
-//
-//    @BeforeEach
-//    public void setup() {
-//        mockMvc = MockMvcBuilders
-//                .webAppContextSetup(context)
-//                .apply(springSecurity())
-//                .build();
-//    }
-//
-//    @Test
-//    void getAllEmployeesWithoutAccess() throws Exception {
-//        this.mockMvc.perform(get("/v1/employees"))
-//                .andExpect(status().isUnauthorized());
-//    }
-//
-//    @Test
-//    void getAllEmployeesAsAdmin() throws Exception {
-//        this.mockMvc.perform(get("/v1/employees")
-//                .with(user("admin")))
-//                .andExpect(status().isOk())
-//                .andExpect(content().encoding(StandardCharsets.ISO_8859_1));
-//    }
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void getAllEmployeesWithoutAccess() throws Exception {
+        this.mockMvc.perform(get("/v1/employees"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void getAllEmployeesAsAdmin() throws Exception {
+        this.mockMvc.perform(get("/v1/employees")
+                .with(user("admin").roles("ADMIN")))
+                .andExpect(status().isOk())
+                .andExpect(content().encoding(StandardCharsets.ISO_8859_1));
+    }
 }
