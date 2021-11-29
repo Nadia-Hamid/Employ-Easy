98a1944dd646d9cf61c975c0aed4bba44f8f62b8
Nadia-Hamid <hamid.nadia@gmail.com>
2021-11-29 11:06:42 +0100

    Addin test to addCustomer and deleteCustomer in Service Layer

--- employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
+++ employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
@@ -1,8 +1,6 @@
 package se.yrgo.employee.controllers;
 
-import static org.hamcrest.CoreMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,16 +9,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -31,68 +25,47 @@ import se.yrgo.employee.services.EmployeeService;
 @WebMvcTest(EmployeeController.class)
 class EmployeeControllerTest {
 
-    @MockBean
-    private EmployeeService mockedEmployeeService;
+	@MockBean
+	private EmployeeService mockedEmployeeService;
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-//    @Test
-//    void getAllEmployees() throws Exception {
-//    	
-//        List<EmployeeDTO> employeeDTOList= new ArrayList<>();
-//        
-//        List<Employee> employeeList= new ArrayList<>();
-//        
-//        Employee emp = new Employee("Marius", "Marthinussen", "890519-XXXX", "Marius@gmail.com", "12345678",
-//                "Sodra Vagen", "44556", "Goteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null);
-//        
-//        employeeList.add(emp);
-//        
-//        EmployeeDTO dto = new EmployeeDTO(emp);
-//        
-//        employeeDTOList.add(dto);
-//        
-//        emp = new Employee("Nadia", "Hamid", "900519-XXXX", "Nadia@gmail.com", "87654321",
-//                "Norra Vagen", "44556", "Goteborg", "developer", "saab", LocalDate.of(2005, 1, 1), null);
-//        
-//        employeeList.add(emp);
-//        
-//        dto = new EmployeeDTO(emp);
-//        
-//        employeeDTOList.add(dto);
-//        
-//        when(mockedEmployeeService.findAll()).thenReturn(employeeList);
-//        
-//        String url="/v1/employees";
-//        MvcResult result = mockMvc.perform(get(url)).andExpect(status().isOk()).andReturn();
-//        String actualResponseJson= result.getResponse().getContentAsString();
-//        String expectedResultJson= objectMapper.writeValueAsString(employeeDTOList);
-//        assertEquals(expectedResultJson,actualResponseJson);
-//    }
-    
-//    @Test
-//    void registerTest() throws Exception{
-//    	
-//    	 Employee emp = new Employee("Marius", "Marthinussen", "890519-XXXX", "Marius@gmail.com", "12345678",
-//                 "Sodra Vagen", "44556", "Goteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null);
-//    	 
-//         EmployeeDTO dto = new EmployeeDTO(emp);
-//
-//    	 this.mockMvc
-//    	 
-//    	.perform(MockMvcRequestBuilders
-//    			.post("/v1/employees/")
-//    			.contentType(MediaType.APPLICATION_JSON)
-//    			.content()	
-//    			)
-//    	.andExpect(MockMvcResultMatchers.status().isCreated())
-//    	.andExpect(MockMvcResultMatchers.header().exists("Location"))
-//    	.andExpect(MockMvcResultMatchers.header().string("Location", Matchers.containsString("marius")));
-//    	 
-//    	 verify(mockedEmployeeService).addEmployee((Employee) any(Employee.class));
-//    }
+	@Test
+	void getAllEmployees() throws Exception {
+
+		List<EmployeeDTO> employeeDTOList = new ArrayList<>();
+
+		List<Employee> employeeList = new ArrayList<>();
+
+		Employee emp = new Employee("Marius", "Marthinussen", "890519-XXXX", "Marius@gmail.com", "12345678",
+				"Sodra Vagen", "44556", "Goteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null);
+
+		employeeList.add(emp);
+
+		EmployeeDTO dto = new EmployeeDTO(emp);
+
+		employeeDTOList.add(dto);
+
+		emp = new Employee("Nadia", "Hamid", "900519-XXXX", "Nadia@gmail.com", "87654321", "Norra Vagen", "44556",
+				"Goteborg", "developer", "saab", LocalDate.of(2005, 1, 1), null);
+
+		employeeList.add(emp);
+
+		dto = new EmployeeDTO(emp);
+
+		employeeDTOList.add(dto);
+
+		when(mockedEmployeeService.findAll()).thenReturn(employeeList);
+
+		String url = "/v1/employees";
+		MvcResult result = mockMvc.perform(get(url)).andExpect(status().isOk()).andReturn();
+		String actualResponseJson = result.getResponse().getContentAsString();
+		String expectedResultJson = objectMapper.writeValueAsString(employeeDTOList);
+		assertEquals(expectedResultJson, actualResponseJson);
+		
+	}
 }
