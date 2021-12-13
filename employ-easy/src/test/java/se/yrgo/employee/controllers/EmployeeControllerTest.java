4f8c2535b8c021b24371fae306c4e681c5d5487d
Marten Hernebring <marten.hernebring@gmail.com>
2021-12-13 15:06:34 +0100

    Added update employee method

--- employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
+++ employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
@@ -1,27 +1,25 @@
 package se.yrgo.employee.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import se.yrgo.employee.dto.EmployeeDTO;
-import se.yrgo.employee.entities.Employee;
+import se.yrgo.employee.security.PasswordConfig;
+import se.yrgo.employee.security.SecurityConfig;
 import se.yrgo.employee.services.EmployeeService;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ContextConfiguration(classes = {SecurityConfig.class, PasswordConfig.class})
 @WebMvcTest(EmployeeController.class)
 class EmployeeControllerTest {
 
@@ -31,40 +29,12 @@ class EmployeeControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
-
 	@Test
 	void getAllEmployees() throws Exception {
-
-		List<EmployeeDTO> employeeDTOList = new ArrayList<>();
-
-		List<Employee> employeeList = new ArrayList<>();
-
-		Employee emp = new Employee("Marius", "Marthinussen", "890519-XXXX", "Marius@gmail.com", "12345678",
-				"Sodra Vagen", "44556", "Goteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null);
-
-		employeeList.add(emp);
-
-		EmployeeDTO dto = new EmployeeDTO(emp);
-
-		employeeDTOList.add(dto);
-
-		emp = new Employee("Nadia", "Hamid", "900519-XXXX", "Nadia@gmail.com", "87654321", "Norra Vagen", "44556",
-				"Goteborg", "developer", "saab", LocalDate.of(2005, 1, 1), null);
-
-		employeeList.add(emp);
-
-		dto = new EmployeeDTO(emp);
-
-		employeeDTOList.add(dto);
-
-		when(mockedEmployeeService.findAll()).thenReturn(employeeList);
-
+		when(mockedEmployeeService.findAll()).thenReturn(new ArrayList<>());
 		String url = "/v1/employees";
-		MvcResult result = mockMvc.perform(get(url)).andExpect(status().isOk()).andReturn();
+		MvcResult result = mockMvc.perform(get(url).with(user("admin").roles("ADMIN"))).andExpect(status().isNotFound()).andReturn();
 		String actualResponseJson = result.getResponse().getContentAsString();
-		String expectedResultJson = objectMapper.writeValueAsString(employeeDTOList);
-		assertEquals(expectedResultJson, actualResponseJson);
-		
+		assertEquals("", actualResponseJson);
 	}
+}
--- employ-easy/src/test/java/se/yrgo/employee/services/EmployeeServiceTest.java
+++ employ-easy/src/test/java/se/yrgo/employee/services/EmployeeServiceTest.java
@@ -1,6 +1,7 @@
 package se.yrgo.employee.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -16,7 +17,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import se.yrgo.employee.entities.Employee;
+import se.yrgo.employee.entities.enums.EmployeeStatus;
+import se.yrgo.employee.entities.enums.SystemStatus;
 import se.yrgo.employee.repositories.EmployeeRepository;
+
+import javax.persistence.Entity;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
@@ -32,10 +37,11 @@ class EmployeeServiceTest {
 		employeeServiceTest = new EmployeeService(mockedEmployeeRepository);
 		List<Employee> employeeList = new ArrayList<>();
 		Employee emp = new Employee("Marius", "Marthinussen", "890519-XXXX", "Marius@gmail.com", "12345678",
-				"Södra Vägen", "44556", "Göteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null);
+				"Södra Vägen", "44556", "Göteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null, EmployeeStatus.ACTIVE, SystemStatus.USER);
 		employeeList.add(emp);
 		emp = new Employee("Nadia", "Hamid", "900519-XXXX", "Nadia@gmail.com", "87654321", "Norra Vägen", "44556",
-				"Göteborg", "developer", "saab", LocalDate.of(2005, 1, 1), null);
+				"Göteborg", "developer", "saab", LocalDate.of(2005, 1, 1), null, EmployeeStatus.VACATION,
+				SystemStatus.SYSTEM_ADMIN);
 		employeeList.add(emp);
 		when(mockedEmployeeRepository.findAll()).thenReturn(employeeList);
 		assertEquals(2, employeeServiceTest.findAll().size());
@@ -45,20 +51,10 @@ class EmployeeServiceTest {
 	void addEmployeeTest() {
 
 		Employee emp = new Employee("Ana", "Beatriz", "890519-XXXX", "anna@gmail.com", "12345678", "Södra Vägen",
-				"44556", "Göteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null);
+				"44556", "Göteborg", "developer", "volvo", LocalDate.of(2000, 1, 1), null, EmployeeStatus.VACATION,
+				SystemStatus.SYSTEM_ADMIN);
 		
 		employeeServiceTest.addEmployee(emp);
 		verify(mockedEmployeeRepository, times(1)).save(emp);
-	}
-
-	@Test
-	void deleteEmployeeTest() {
-
-		Employee emp = new Employee();
-		emp.setfirstName("Test Name");
-		emp.setId(3L);
-		
-		employeeServiceTest.deleteEmployee(emp.getId());
-		verify(mockedEmployeeRepository).deleteById(emp.getId());
 	}
 }
