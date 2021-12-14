aff035734158d565e9a7dc2b38334b42a61fae01
Nadia-Hamid <hamid.nadia@gmail.com>
2021-12-14 16:53:52 +0100

    adding new controller

--- employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
+++ employ-easy/src/test/java/se/yrgo/employee/controllers/EmployeeControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
 import se.yrgo.employee.security.PasswordConfig;
 import se.yrgo.employee.security.SecurityConfig;
 import se.yrgo.employee.services.EmployeeService;
