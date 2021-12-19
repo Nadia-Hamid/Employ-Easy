5e7a74234a1d164425e1e9aa448fffe2764f07bb
Aiste P <72742124+aistepk@users.noreply.github.com>
2021-12-19 13:08:52 +0100

    adding user view

--- employee-manager-app/src/app/app-routing.module.ts
+++ employee-manager-app/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+
 
 const routes: Routes = [
     { path:"", redirectTo:"login", pathMatch:"full" },
     { path: 'login', component: LoginComponent },
-    { path: 'v1/employees', component: EmployeeComponent }
+    { path: 'v1/employees', component: EmployeeComponent },
+    { path: 'v1/employees/user', component: UserComponent }
 ];
 
 @NgModule({
---
+++ employee-manager-app/src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core'
+import { Router, ActivatedRoute } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { EmployeeService } from '../services/employee.service'
+import { Employee } from '../employee/employee'
+import { HttpErrorResponse } from '@angular/common/http'
+
+@Component({ templateUrl: 'user.component.html', styleUrls: ['./user.component.css'] })
+export class UserComponent implements OnInit {
+  public employee?: Employee
+
+  constructor(private router: Router, private employeeService: EmployeeService) {}
+
+  go() {
+    this.router.navigate(['v1/employees/user'])
+  }
+
+  ngOnInit(): void {
+    this.getOneEmployee()
+  }
+
+  getOneEmployee(): void {
+    this.employeeService.getOneEmployee('marmar9110').subscribe(
+      (response: Employee) => {
+        this.employee = response
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+}
