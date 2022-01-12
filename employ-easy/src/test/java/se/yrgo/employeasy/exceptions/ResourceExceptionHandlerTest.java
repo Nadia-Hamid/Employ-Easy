f0de99257786dc755d3628c94cc56388b45fde73
Marten Hernebring <marten.hernebring@gmail.com>
2022-01-12 09:56:27 +0100

    Added test for throwing conflict, nullpointer and illegalarg exceptions. Minor refact

--- employ-easy/src/test/java/se/yrgo/employeasy/exceptions/ResourceExceptionHandlerTest.java
+++ employ-easy/src/test/java/se/yrgo/employeasy/exceptions/ResourceExceptionHandlerTest.java
@@ -2,33 +2,55 @@ package se.yrgo.employeasy.exceptions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ResourceExceptionHandlerTest {
 
+    private final ResourceExceptionHandler handlerTest = new ResourceExceptionHandler();
+
     @Test
-    void objectNotFoundTest() {
-        ResourceExceptionHandler reh = new ResourceExceptionHandler();
+    void throwObjectNotFound() {
         try{
-            throw new ObjectNotFoundException("test");
+            throw new ObjectNotFoundException("try to catch me");
         } catch (ObjectNotFoundException ex) {
-            var response = reh.objectNotFound(ex);
+            ResponseEntity<StandardError> response = handlerTest.objectNotFound(ex);
             assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
             assertEquals("Not Found", response.getBody().getError());
         }
     }
 
     @Test
-    void conflict() {
+    void throwConflict() {
+        try{
+            throw new ConflictException("try to catch me");
+        } catch (ConflictException ex) {
+            ResponseEntity<StandardError> response = handlerTest.conflict(ex);
+            assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+            assertEquals("Conflict", response.getBody().getError());
+        }
     }
 
     @Test
-    void nullPointer() {
+    void throwNullPointer() {
+        try{
+            throw new NullPointerException("try to catch me");
+        } catch (NullPointerException ex) {
+            ResponseEntity<StandardError> response = handlerTest.nullPointer(ex);
+            assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+            assertEquals("Null Pointer", response.getBody().getError());
+        }
     }
 
     @Test
-    void illegalArgument() {
+    void throwIllegalArgument() {
+        try {
+            throw new IllegalArgumentException("try to catch me");
+        } catch (IllegalArgumentException ex) {
+            ResponseEntity<StandardError> response = handlerTest.illegalArgument(ex);
+            assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+            assertEquals("Illegal Argument", response.getBody().getError());
+        }
     }
 }
